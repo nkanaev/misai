00f7fd4a24090a0d8124c02efe38ff4f1f659b34
Nazar Kanaev <nkanaev@live.com>
2018-12-24 21:40:28 +0000

    loop scope test

--- test/test_loops.py
+++ test/test_loops.py
@@ -14,3 +14,11 @@ def test_simple():
 def test_loop_filters():
     t = Template('{{ for a in b | reverse }}{{ a }}{{ end }}')
     assert t.render(b=['foo', 'bar']) == 'barfoo'
+
+
+def test_scopes():
+    t = Template(
+        '{{ set foo = "baz" }}'
+        '{{ for foo in items }}{{ foo }}{{ end }}'
+        '{{ foo }}')
+    assert t.render(items=['bar']) == 'barbaz'
