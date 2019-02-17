c49e2f85a4095fb0c44fc09152ea15a7ea147dea
Nazar Kanaev <nkanaev@live.com>
2019-02-17 22:33:19 +0000

    v0.1.0

--- setup.py
+++ setup.py
@@ -2,23 +2,29 @@
 from setuptools import setup
 
 
+long_description = open('readme.rst').read()
+
+
 setup(
     name='misai',
-    version='0.0.1',
+    version='0.1.0',
     description='simple template engine',
+    long_description=long_description,
     url='https://github.com/nkanaev/misai',
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     author='Nazar Kanaev',
     author_email='nkanaev@live.com',
     py_modules=['misai'],
-    test_suite='tests'
 )
