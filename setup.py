f67b97254d3aed0518d5dc6cd1e9e57fb4a0bdeb
Nazar Kanaev <nkanaev@live.com>
2019-05-01 19:18:04 +0100

    update

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 # coding: utf-8
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 
-long_description = open('readme.rst').read()
+class PyTestCommand(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+
+    def run_tests(self):
+        import sys, pytest
+        sys.exit(pytest.main([]))
 
 
 setup(
     name='misai',
     version='0.1.0',
     description='simple template engine',
-    long_description=long_description,
+    long_description=open('readme.rst').read(),
     url='https://github.com/nkanaev/misai',
     license='MIT',
     classifiers=[
@@ -27,4 +33,7 @@ setup(
     author='Nazar Kanaev',
     author_email='nkanaev@live.com',
     py_modules=['misai'],
+    test_suite='test',
+    tests_require=['pytest'],
+    cmdclass={"test": PyTestCommand},
 )
